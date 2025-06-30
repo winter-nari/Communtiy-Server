f95fa8051a9732a27b46279daa2f1d761a99677f
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-01 03:23:13 +0900

    user-service

---
+++ src/main/java/com/kjone/useroauth/UserOauthApplication.java
@@ -0,0 +1,13 @@
+package com.kjone.useroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjone/useroauth/UserOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjone.useroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
