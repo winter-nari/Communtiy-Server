492d12204266cb9d204f04bc22fd91d72b4654d3
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-02 15:24:11 +0900

    feat: 수정

--- src/main/java/com/kjone/useroauth/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/kjone/useroauth/security/jwt/JwtAuthenticationFilter.java
@@ -1,10 +1,12 @@
 package com.kjone.useroauth.security.jwt;
 
+import com.kjone.useroauth.security.service.CustomUserDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,10 +14,9 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.kjone.useroauth.security.service.CustomUserDetailService;
-
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -36,14 +37,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
                 if (userId != null && SecurityContextHolder.getContext().getAuthentication() == null) {
                     UserDetails userDetails = customUserDetailService.loadUserById(userId);
-                    var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
                     authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             }
         } catch (Exception e) {
-            // 인증 실패 로그 남기거나 응답 처리 (필요시)
-            logger.error("Could not set user authentication in security context", e);
+            log.error("JWT 인증 실패: {}", e.getMessage());
         }
 
         filterChain.doFilter(request, response);
