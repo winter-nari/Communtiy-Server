c51466b7d2caafa7862a37a6882a22fa8c350062
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-02 15:24:42 +0900

    feat: 리프레쉬 토큰 엔티티 add

---
+++ src/main/java/com/kjone/useroauth/entity/RefreshTokenEntity.java
@@ -0,0 +1,29 @@
+package com.kjone.useroauth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.Date;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshTokenEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    private String token;
+
+    private Date expiryDate;
+}
