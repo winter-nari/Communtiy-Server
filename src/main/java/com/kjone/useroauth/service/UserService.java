2cb1ff060d220bcda120b9e4698905974d4e46fc
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-02 15:26:48 +0900

    feat: 리프레시토큰 메서드 추가

--- src/main/java/com/kjone/useroauth/service/UserService.java
+++ src/main/java/com/kjone/useroauth/service/UserService.java
@@ -6,10 +6,11 @@ import com.kjone.useroauth.request.LoginRequest;
 import com.kjone.useroauth.request.SignRequest;
 import com.kjone.useroauth.response.LoginResponse;
 import com.kjone.useroauth.response.SignResponse;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface UserService {
     public boolean signUp(SignRequest signRequest) throws Exception;
-    public LoginResponse signIn(LoginRequest loginRequest) throws Exception;
-
+    public LoginResponse signIn(LoginRequest loginRequest, HttpServletResponse response) throws Exception;
+    LoginResponse refreshToken(String refreshToken, HttpServletResponse response) throws Exception;
 
 }
