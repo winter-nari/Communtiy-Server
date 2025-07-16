b489e24237a3400e66a6ff640e8a0c2d80c9f797
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-16 17:59:41 +0900

    feat: configure WebSocket with STOMP and authentication interceptor

--- src/main/java/com/kjone/useroauth/global/config/WebSocketConfig.java
+++ src/main/java/com/kjone/useroauth/global/config/WebSocketConfig.java
@@ -1,29 +1,33 @@
-package com.kjone.useroauth.config;
+package com.kjone.useroauth.global.config;
 
+import com.kjone.useroauth.global.security.jwt.JwtTokenProvider;
+import com.kjone.useroauth.global.security.service.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-/**
- * Created by rajeevkumarsingh on 24/07/17.
- */
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailService customUserDetailService;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://f270e5f29176.ngrok.app") // 또는 ngrok 주소 명시
+                .setAllowedOrigins("http://localhost:3000", "http://localhost:8080") // 주소 명시
+                .addInterceptors(new AuthHandshakeInterceptor(jwtTokenProvider, customUserDetailService))  // 인증 인터셉터 추가
                 .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");   // Enables a simple in-memory broker
+        registry.enableSimpleBroker("/topic");
 
     }
 }
