a888d2fb209aa95ef1381336fe5b0be9469ed20f
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-16 17:59:26 +0900

    feat: configure CORS and static resource handling

--- src/main/java/com/kjone/useroauth/global/config/WebConfig.java
+++ src/main/java/com/kjone/useroauth/global/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.kjone.useroauth.config;
+package com.kjone.useroauth.global.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")  // 모든 도메인 허용
+                .allowedOriginPatterns("http://localhost:3000")  // 프론트엔드 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
