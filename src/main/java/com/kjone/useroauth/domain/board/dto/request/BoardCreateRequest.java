88f80501ad39ea355b894e957a5b58ba05eed405
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-16 17:44:45 +0900

    feat: Add BoardCreateRequest DTO for board creation

--- src/main/java/com/kjone/useroauth/domain/board/dto/request/BoardCreateRequest.java
+++ src/main/java/com/kjone/useroauth/domain/board/dto/request/BoardCreateRequest.java
@@ -1,11 +1,12 @@
-package com.kjone.useroauth.dto;
+package com.kjone.useroauth.domain.board.dto.request;
 
 
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
-public class BoardCreateRequestDto {
+@Getter
+@Setter
+public class BoardCreateRequest {
     private String name;
     private String description;
     private MultipartFile image;
