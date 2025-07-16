e57e061515574e14433210c41255a49879267f03
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-16 17:48:08 +0900

    feat: Add ChatRoom entity with participants and messages relationships

---
+++ src/main/java/com/kjone/useroauth/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,43 @@
+package com.kjone.useroauth.domain.chat.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_chat_room")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name; // 그룹이면 이름, 1:1이면 null
+
+    @Builder.Default
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<UserRoom> participants = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<ChatMessages> messages;
+
+    // 양방향 연관관계 편의 메서드
+    public void addParticipant(UserRoom userRoom) {
+        participants.add(userRoom);
+        userRoom.setChatRoom(this);
+    }
+
+    public void removeParticipant(UserRoom userRoom) {
+        participants.remove(userRoom);
+        userRoom.setChatRoom(null);
+    }
+}
+
