5d5b64e13ed0ce1c358fe2ad2f6fb90250937ca7
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-16 17:58:07 +0900

    feat: add UserRepository interface for UserEntity

--- src/main/java/com/kjone/useroauth/domain/oauth/repository/UserRepository.java
+++ src/main/java/com/kjone/useroauth/domain/oauth/repository/UserRepository.java
@@ -1,11 +1,10 @@
-package com.kjone.useroauth.repository;
+package com.kjone.useroauth.domain.oauth.repository;
 
-import com.kjone.useroauth.entity.UserEntity;
+import com.kjone.useroauth.domain.oauth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
