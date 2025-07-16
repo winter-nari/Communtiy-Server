88d2c199ad3e0cffd530542ba7cd2d0d38839caf
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-16 17:49:59 +0900

    feat: Extend UserDetailsService with loadUserById method in CustomUserDetailService

--- src/main/java/com/kjone/useroauth/global/security/service/CustomUserDetailService.java
+++ src/main/java/com/kjone/useroauth/global/security/service/CustomUserDetailService.java
@@ -1,11 +1,13 @@
-package com.kjone.useroauth.security.service;
+package com.kjone.useroauth.global.security.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface CustomUserDetailService extends UserDetailsService {
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
+
+    // Username으로 조회
+    UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
 
     // 추가: userId로 조회
     UserDetails loadUserById(String userId) throws UsernameNotFoundException;
