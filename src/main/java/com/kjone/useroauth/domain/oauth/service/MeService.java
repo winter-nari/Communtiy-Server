45d59522d7fa548ba6cdeecf469aba0ce954d8ec
jeowon_1 <rlawjddnsj8371@gmail.com>
2025-07-16 17:54:54 +0900

    feat: add MeService interface with me() method to get current user info

---
+++ src/main/java/com/kjone/useroauth/domain/oauth/service/MeService.java
@@ -0,0 +1,7 @@
+package com.kjone.useroauth.domain.oauth.service;
+
+import com.kjone.useroauth.domain.oauth.dto.response.MemberResponse;
+
+public interface MeService {
+    public MemberResponse me();
+}
